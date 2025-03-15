889994c4845c58e9d3bb5a17e2f986c89aa7b9a4
myownarea <1131905436@qq.com>
2025-03-15 22:07:48 +0800

    pinia优化导航数据

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,17 +1,7 @@
 <script setup>
-  import {getCategoryAPI} from '@/apis/layout'
-  import {onMounted, ref} from 'vue'
-
-  const categoryList = ref([])
-  const getCategory =async () =>{
-   const res = await getCategoryAPI()
-   console.log(res)
-   categoryList.value = res.result
-  }
-
-  onMounted(() => {
-    getCategory()
-    })
+import {useCategoryStore} from '@/stores/category'
+//使用pinia中的数据
+const categoryStore = useCategoryStore()
 </script>
 
 <template>
@@ -21,7 +11,7 @@
         <RouterLink to="/">小兔鲜</RouterLink>
       </h1>
       <ul class="app-header-nav">
-        <li class="home" v-for="item in categoryList" :key="item.id" >
+        <li class="home" v-for="item in categoryStore.categoryList" :key="item.id" >
           <RouterLink to="/">{{item.name}}</RouterLink>
         </li>
 
