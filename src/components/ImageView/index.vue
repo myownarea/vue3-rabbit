acd4130b47a517bd095a9b5154bd97762da64abc
myownarea <1131905436@qq.com>
2025-04-01 11:07:33 +0800

    详情页-放大镜效果-滑块跟随鼠标移动

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref } from 'vue'
+import { ref,watch } from 'vue'
+import { useMouseInElement } from '@vueuse/core'
 // 图片列表
 const imageList = [
   "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
@@ -14,16 +15,63 @@ const activeIndex = ref(0)
 const enterhandler = (i) =>{
   activeIndex.value = i
 }
+
+//2.获取鼠标相对位置
+const target = ref(null)
+const {elementX, elementY,isOutside} = useMouseInElement(target)
+
+//3.控制滑块跟随鼠标移动，监听elementX/Y变化,一旦变化，就控制滑块的left/top属性变化
+const left = ref(0)
+const top = ref(0)
+
+const positionX = ref(0)
+const positionY = ref(0)
+watch([elementX, elementY,isOutside], () => {
+  // console.log('xy变化了')
+  if(isOutside.value) return
+  console.log('后续逻辑执行了')
+  //有效范围内控制滑块距离
+  //横向
+  if(elementX.value > 100 && elementX.value < 300)  {
+    left.value = elementX.value - 100
+  }
+  //纵向
+  if(elementY.value > 100 && elementY.value < 300)  {
+    top.value = elementY.value - 100
+  }
+
+  //处理边界
+  if(elementX.value > 300){
+    left.value = 200
+  }
+  if(elementX.value < 100){
+    left.value = 0
+  }
+  if(elementY.value > 300){
+    top.value = 200
+  }
+  if(elementY.value < 100){
+    top.value = 0
+  }
+
+  //控制大图的显示
+  positionX.value = -left.value * 2
+  positionY.value = -top.value * 2
+
+})
+
 </script>
 
 
 <template>
+  {{ elementX }}, {{ elementY }}, {{ isOutside }}
   <div class="goods-image">
     <!-- 左侧大图-->
-    <div class="middle" ref="target">
+
+    <div class="middle" ref="target" :ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer" v-show="!isOutside" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
